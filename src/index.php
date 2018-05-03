7ad162eceb7fd90d7f27eacb438c4fdd596144b9
Jose Yapur <jose@Joses-MacBook-Air.local>
2018-05-03 16:19:04 -0500

    Limpieza de demo

--- src/index.php
+++ src/index.php
@@ -1,17 +1,4 @@
 <?php
+echo '<h1>Hola Mundo</h1>';
 phpinfo();
-$file = '/aci/logs/log.txt';
-if(is_file($file))
-{
-    // Open the file to get existing content
-    $current = file_get_contents($file);
-    // Append a new person to the file
-    $current .= date("Y-m-d").'\t'.$_SERVER['REMOTE_ADDR']."\n";
-    // Write the contents back to the file
-    file_put_contents($file, $current);
-}
-else
-{
-    file_put_contents($file, "");
-}
 ?>
