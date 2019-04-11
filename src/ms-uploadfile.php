72fc75d43a63808f1d25a6dffcb9fa0a083b1c6f
Jose Yapur <yapur@outlook.com>
2019-04-11 00:42:30 -0500

    Update ms-uploadfile.php

--- src/ms-uploadfile.php
+++ src/ms-uploadfile.php
@@ -1,19 +1,11 @@
 <?php
 
 $dir_subida = 'img/';
-$fichero_subido = $dir_subida . basename($_FILES['fileToUpload']['name']);
+$fichero_subido = $dir_subida . $_FILES['fileToUpload']['name'];
 
-echo '<pre>';
 if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $fichero_subido)) {
-    echo "El fichero es válido y se subió con éxito.\n";
+    header("Location: subirAzure.php?archivo=" . $_FILES["fileToUpload"]["name"]);
 } else {
-    echo "¡Posible ataque de subida de ficheros!\n";
+    
 }
-
-echo 'Más información de depuración:';
-print_r($_FILES);
-
-print "</pre>";
-
-header("Location: subirAzure.php?archivo="+$_FILES["fileToUpload"]["name"]);
 ?>
